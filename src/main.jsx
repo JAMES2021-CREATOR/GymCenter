18e2effe097cf2186bf1b317b813f3ca42b5e596
JAMES2021-CREATOR <dahunsijames2021@email.com>
2026-04-18 10:37:23 +0100

    Fix errors

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import Footer from "./component/Footer.jsx";
+import Footer from "./component/footer.jsx";
 import Navbar from "./component/Navbar.jsx";
 
 createRoot(document.getElementById("root")).render(
